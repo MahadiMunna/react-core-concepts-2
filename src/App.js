e4c12b5b5777d87a9ab19e10d7bb6e9b389655c8
MahadiMunna <mahadimunna.official@gmail.com>
2022-09-20 16:32:58 +0600

    mapping of a object and props

--- src/App.js
+++ src/App.js
@@ -2,24 +2,34 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const products = [
+    {name: 'laptop', price : 53000},
+    {name: 'phone', price : 93000},
+    {name: 'watch', price : 3000},
+    {name: 'tablet', price : 43000}
+  ]
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    {
+      products.map(product=><Product name={product.name} price = {product.price}></Product>)
+    }
+
+      {/* <Product name="Laptop" price="200000"></Product>
+      <Product name="Iphone" price="150000"></Product> */}
     </div>
   );
+}
+
+function Product(props){
+  return (
+    <div className='product'>
+      <h3>Name: {props.name} </h3>
+      <p>Price: {props.price}</p>
+    </div>
+  )
 }
 
 export default App;
